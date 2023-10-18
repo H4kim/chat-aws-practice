7de374c2cd36f48bb15bd5469406f95c56ad7146
Hakim <ozil.hakim@gmail.com>
2023-10-17 21:57:07 -0400

    dockerize

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,14 @@ import express from "express";
 import http from "http";
 import WebSocketManager from "./websocket/WebsocketManager";
 import chatHandler from "./wsHandlers/chat";
+import path from "path";
 
 const app = express();
 const server = http.createServer(app);
 const wssManager = new WebSocketManager(server);
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
 
 new chatHandler(wssManager);
 
